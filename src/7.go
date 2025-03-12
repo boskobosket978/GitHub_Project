d86c94d7c4de61a58987106f1ea92df6160e6db0
boskobosket978 <boskobosket978@gmail.com>
2025-03-12 04:25:21 +0000

    Add 7.go

---
+++ src/7.go
@@ -0,0 +1,10 @@
+
+// This function takes in an integer and returns its square root
+func calculateSquareRoot(num int) float64 {
+    return math.Sqrt(float64(num))
+}
+
+// This function takes in a string and returns its length
+func calculateStringLength(str string) int {
+    return len(str)
+}
